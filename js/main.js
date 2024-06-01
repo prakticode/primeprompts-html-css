304d19ce8e7e70523b3963eaf099f8c2cdec9ebb
imadbkr <kariimad@yahoo.fr>
2024-06-01 12:09:04 +0200

    Change order elements JS

--- js/main.js
+++ js/main.js
@@ -18,6 +18,17 @@ const handleMobileMenu = () => {
 };
 
 const animateMainContent = () => {
+    const elementsToAnimate = [
+        { selector: ".main__hero-title", transform: false },
+        { selector: ".main__hero-description", transform: false },
+        { selector: ".main__hero-mobile-description", transform: false },
+        { selector: ".main__hero-button", transform: false },
+        ...Array.from(document.querySelectorAll(".main__feature")).map((feature, index) => ({
+            element: feature,
+            transform: true
+        }))
+    ];
+
     const animateElement = (element, delay = 0, transform = false) => {
         element.style.opacity = 0;
         if (transform) {
@@ -31,17 +42,6 @@ const animateMainContent = () => {
             }
         }, delay);
     };
-
-    const elementsToAnimate = [
-        { selector: ".main__hero-title", transform: false },
-        { selector: ".main__hero-description", transform: false },
-        { selector: ".main__hero-mobile-description", transform: false },
-        { selector: ".main__hero-button", transform: false },
-        ...Array.from(document.querySelectorAll(".main__feature")).map((feature, index) => ({
-            element: feature,
-            transform: true
-        }))
-    ];
 
     elementsToAnimate.forEach((item, index) => {
         const element = item.element || document.querySelector(item.selector);
